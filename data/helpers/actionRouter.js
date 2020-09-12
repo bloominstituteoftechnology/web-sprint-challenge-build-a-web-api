e859c66bf3ea1a8344e9f82359dd102694f2dfe2
Aeson Johnson <aeson.m.johnson@gmail.com>
2020-09-11 20:45:59 -0700

    updating actionRouter.js

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -1,15 +1,36 @@
 const express = require("express");
-const data = require("./actionModel.js");
+const actions = require("./actionModel.js");
 const router = express.Router();
 
-// id / name / description / completed
-
 //get
+router.get("/:id", validateId, (req, res) => {
+    actions.get(req.params.id)
+        .then(action => {
+            res.status(200).json(action)
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(404).json({ errorMessage: "Action ID not found" })
+        })
+})
 
 //post
 
 //delete
 
 //put
+
+// custom middleware
+function validateId(req, res, next) {
+    actions.get(req.params.id)
+    .then(action => {
+        if(action) {
+            req.action = action;
+            next();
+        } else {
+            res.status(404).json({ message: "Invalid Action ID." })
+        }
+    })
+}
 
 module.exports = router;
