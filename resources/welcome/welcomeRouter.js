2c3be17d4e43e6dfd42ae578819db28954693ade
Bruno Paula <devbrunopaula@gmail.com>
2020-10-09 21:24:07 -0400

    Add .ENV file

--- resources/welcome/welcomeRouter.js
+++ resources/welcome/welcomeRouter.js
@@ -1,9 +1,15 @@
 const express = require('express')
 
 const router = express.Router()
-
+console.log(process.env.name)
 router.get('/', (req, res) => {
-  res.send(`<h2>Welcome To Bruno's API 1.0!!</h2>`)
+  res.send(
+    `<div>
+    <h2>Welcome To ${process.env.name} </h2>
+    <p> API version: ${process.env.npm_package_version}!! </p>
+    </div>
+    `
+  )
 })
 
 module.exports = router
