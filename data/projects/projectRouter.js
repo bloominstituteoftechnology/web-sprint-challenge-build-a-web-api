4d485bc153a34df606c402fbe79a8b077cbe4904
Elizabeth Hagag <lizjanegiles@gmail.com>
2020-10-10 18:36:13 -0500

    finished project router

---
+++ data/projects/projectRouter.js
@@ -0,0 +1,110 @@
+const express = require('express')
+
+const projectDb = require('./projectModel')
+const actionRouter = require('../actions/actionModel')
+const router = express.Router()
+
+router.use(express.json())
+
+router.get('/',  (req, res) => {
+    projectDb.get()
+
+    .then((pro) => {
+        res.status(200).json(pro)
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json({
+            message: "could not retrive project"
+        })
+    })
+})
+
+router.post('/',  validateProject, (req, res) => {
+    const projectData = req.body
+    if (!projectData.name || !projectData.description) {
+        res.status(500).json({ message: "Name and Desc needed" });
+    } else {
+        projectDb.insert(projectData)
+        .then((pro) => {
+            res.status(201).json({pro})
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json({
+                message: "could not post project"
+            })
+        })
+    }
+} )
+
+router.get('/:id', validateProjectID, (req,res) => {
+    const id = req.params.id
+    projectDb.get(id)
+
+    .then((pro) => {
+        res.status(200).json(pro)
+    })
+    .catch((err)=> {
+        console.log(err)
+        res.status(500).json({
+            message: "could not retrive user"
+        })
+    })
+})
+
+router.put('/:id', validateProjectID, validateProject, (req, res) => {
+    const id = req.params.id
+    const data = req.body
+
+    projectDb.update(id, data)
+    .then((pro) => {
+        res.status(200).json(pro)
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json({
+            message: "could not add project"
+        })
+    })
+})
+
+router.delete('/:id', validateProjectID, (req, res) => {
+    const id = req.params.id
+    projectDb.remove(id)
+    .then((pro) => {
+        res.status(200).json(pro)
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json({
+            message: "could"
+        })
+    })
+})
+
+function validateProjectID(req, res, next) {
+    const proId = req.params.id
+    projectDb.get(proId)
+
+    .then((pro) => {
+        if(pro) {
+            req.pro = pro
+            next()
+        } else {
+            res.status(400).json({messege: "invalid"})
+        }
+    })
+}
+
+function validateProject(req, res, next) {
+    const projectData = req.body
+  
+    if (!projectData.name || !projectData.description) {
+        res.status(500).json({ message: "Name and Desc needed" });
+    } else {
+      next()
+    }
+  }
+
+module.exports = router;
