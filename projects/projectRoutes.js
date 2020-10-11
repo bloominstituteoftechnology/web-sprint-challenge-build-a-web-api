233a5ddaa4e11b6e184684eaddaa2c55f81cad7d
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-11 09:56:32 -0400

    Created get endpoint for /projects

--- projects/projectRoutes.js
+++ projects/projectRoutes.js
@@ -4,7 +4,7 @@ const db = require("../data/helpers/projectModel");
 
 router.get("/", (req, res) => {
     db.get().then(projects=>{
-        console.log(projects);
+        res.status(200).json(projects);
     }).catch(err=>{
         res.status(500).json({message: "A server error occurred"});
     });
