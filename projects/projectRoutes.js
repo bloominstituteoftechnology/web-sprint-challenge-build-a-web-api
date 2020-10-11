de5035d79ff22a6544a67609af5f78a4f6ee6b17
Natividad Alvarez <tivialvarez@gmail.com>
2020-10-11 10:04:25 -0400

    Created get and put endpoints for /projects/:id

--- projects/projectRoutes.js
+++ projects/projectRoutes.js
@@ -8,6 +8,26 @@ router.get("/", (req, res) => {
     }).catch(err=>{
         res.status(500).json({message: "A server error occurred"});
     });
-})
+});
+
+router.get("/:id", (req, res)=>{
+    db.get(req.params.id).then(project=>{
+        if(!project) return res.status(404).json({message: "Project doesn't exist"})
+        res.status(200).json(project);
+    }).catch(err=>{
+        res.status(500).json({message: "A server error occurred"});
+    })
+});
+
+router.put("/:id", (req, res)=>{
+    const changes = req.body;
+    if(!changes) res.status(400).json({messages: "Required info missing"});
+    db.update(req.params.id, changes).then(updatedProject=>{
+        if(!updatedProject) return res.status(404).json({message: "Project does not exist"});
+        res.status(201).json(updatedProject);
+    }).catch(err=>{
+        res.status(500).json({message: "A server error occurred"});
+    })
+});
 
 module.exports = router;
