d6cf073d50491da4f543bffbfe4a2d8f7cf071d1
Virginia P. Scirrotto <c0d3.vp@gmail.com>
2020-10-30 10:50:04 -0700

    added dotenv, which was missing from my project, therefore throwing errors

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
-require('dotenv').config();
+//require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 1911;
 server.listen(port, () => {
   console.log(`\n* Server Running on http://localhost:${port} ...*\n`)
 });
