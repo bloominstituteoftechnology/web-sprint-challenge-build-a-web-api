36f2e7d0ede52cf1a7c21b7e1e519821334b4c74
Rigo <limon1285@gmail.com>
2020-11-09 23:22:04 -0600

    added ENV file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 const server = require("./server.js");
 
-const port = 4000;
+const port = process.env.PORT;
 
 server.listen(port, () => {
-  console.log(`\n***server is running on http://localhost:${port}\n`);
+  console.log(`\n***server i s running on http://localhost:${port}\n`);
 });
