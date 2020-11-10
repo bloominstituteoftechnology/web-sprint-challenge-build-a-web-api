1481dca49bee40bfca7262f377ebddb91ddf464c
Michael Sanchez <michaelsanchez42@live.com>
2020-11-09 22:11:13 -0700

    mvp

--- actionRouter.js
+++ actionRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const actionModel = require("./data/helpers/actionModel ");
+const actionModel = require("./data/helpers/actionModel");
 
 const router = express.Router();
 
