d75e465568d43f73dd04da4eb91d9bf60db0d670
LBU <lbu@LBUui-MacBook-Pro.local>
2020-12-04 12:36:12 -0500

    routers finished, met MVP

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,91 @@
 // Write your "projects" router here!
+const express = require('express')
+const router = express.Router()
+const Project = require('./projects-model')
+    
+
+router.get('/', (req, res) => {
+    Project.get()
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: err.message })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    Project.get(id)
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: err.message })
+        })
+})
+
+
+router.post('/', (req, res) => {
+    if(!req.body.name || !req.body.description){
+        res.status(400).json({ errorMessage: 'missing required field'})
+    }
+    Project.insert(req.body)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: err.message })
+        })
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const changes = req.body
+    if(!req.body.name || !req.body.description){
+        res.status(400).json({ errorMessage: 'missing required field'})
+    }
+
+    Project.update(id, changes)
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: err.message })
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    Project.remove(id)
+    .then(project => {
+        res.status(200).json({ message: "project deleted"})
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: 'project could not be deleted'})
+    })
+})
+
+router.get('/:id/actions', (req, res) => {
+    const { id } = req.params
+    Project.getProjectActions(id)
+        .then(projectId => {
+            if(projectId){
+                res.status(200).json(projectId)
+            }
+            else{
+                res.status(404).json({ errorMessage: 'could not find the action' })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: err.message })
+        })
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const server = express();
+const actionsRouter = require('./actions/actions-router')
+const projectsRouter = require('./projects/projects-router')
+
+server.get('/', (req, res) => {
+    res.send('hello there')
+})
+server.use(express.json())
+server.use('/api/actions', actionsRouter)
+server.use('/api/projects', projectsRouter)
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
