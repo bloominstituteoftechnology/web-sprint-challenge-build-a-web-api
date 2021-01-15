50077cabac2b48be869ede413585679baf64b047
Ian Luchini <iancarloluchini@gmail.com>
2021-01-15 12:08:28 -0500

    completed GET /api/actions/

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,24 +1,43 @@
 const express = require('express');
+const Action = require('./actions-model');
 const router = express.Router();
 
-// router.get('/' async (req, res) => {
+// curl http://localhost:5000/api/actions
+router.get('/', async (req, res, next) => {
+    try {
+        const actions = await Action.get();
+        res.status(200).json(actions);
+    } catch (err) {
+        next(err);
+    }
+});
 
+// router.get('/:id', async (req, res) => {
+    // try {
+
+    // } catch (err) {
+        
+    // }
 // });
 
-// router.get('/:id' async (req, res) => {
-
-// });
-
-// router.post('/' async (req, res) => {
+// router.post('/', async (req, res) => {
     
 // });
 
-// router.put('/:id' async (req, res) => {
+// router.put('/:id', async (req, res) => {
     
 // });
 
-// router.delete('/:id' async (req, res) => {
+// router.delete('/:id', async (req, res) => {
     
 // });
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        info: 'Something went wrong with the router',
+        message: err.message,
+        stack: err.stack
+    });
+})
 
 module.exports = router;
