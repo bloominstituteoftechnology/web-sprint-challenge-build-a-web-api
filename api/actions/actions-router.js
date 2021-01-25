68b15da1548fd9e5824eb23d3a8253dbd0967daa
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2021-01-24 20:14:40 -0500

    set up router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,3 +4,26 @@ const express = require('express');
 const actions = require('./actions-model');
 
 const router = express.Router();
+
+
+router.get('/api/actions' ,(req, res) => {
+
+})
+
+router.get('/api/actions/:id' ,(req, res) => {
+
+})
+
+router.post('/api/actions' ,(req, res) => {
+
+})
+
+router.put('/api/actions/:id' ,(req, res) => {
+
+})
+
+router.delete('/api/actions/:id' ,(req, res) => {
+
+})
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -5,3 +5,26 @@ const express = require('express');
 const projects = require('./projects-model');
 
 const router = express.Router();
+
+router.get('/api/projects' ,(req, res) => {
+
+})
+
+router.get('/api/projects/:id' ,(req, res) => {
+
+})
+
+router.post('/api/projects' ,(req, res) => {
+
+})
+
+router.put('/api/projects/:id' ,(req, res) => {
+
+})
+
+router.delete('/api/projects/:id' ,(req, res) => {
+
+})
+
+
+module.exports = router;
