b6f3b31f31e461a3959fc6cfe4caabe07ee21681
Diego Roman <diegormnv209@gmail.com>
2021-02-12 08:34:09 -0800

    code added to deploy on heroku

--- api/server.js
+++ api/server.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const helmet = require('helmet')
+
 const server = express();
+
 const actionsRouter = require('./actions/actions-router')
 const projectsRouter = require('./projects/projects-router')
 
 server.use(helmet());
 server.use(express.json())
+
 server.use('/api/actions', actionsRouter)
 server.use('/api/projects', projectsRouter)
+
+server.get('/', (req,res) =>{
+    res.status(200).json({message: 'API running', env: process.env.NODE_ENV})
+})
 
 
 // Complete your server here!
--- index.js
+++ index.js
@@ -12,10 +12,11 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
-
+//eslint-disable-next-line
+const dotenv = require('dotenv').config()
 const server = require('./api/server')
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () =>{
     console.log(`\n* Server Running on http://localhost:${PORT} *\n`)
