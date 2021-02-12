82e72f35919096f3cfbabb16da221e840e70d6bf
Diego Roman <diegormnv209@gmail.com>
2021-02-12 02:18:41 -0800

    delete for projects work

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -21,6 +21,7 @@ router.get('/:id', mw.validateProjectId, (req, res) =>{
     res.status(200).json(req.project)
 })
 
+//POST/ Create a new Project
 router.post('/', mw.validateProject, (req, res) =>{
     Projects.insert(req.body)
     .then((project) =>{
@@ -31,13 +32,23 @@ router.post('/', mw.validateProject, (req, res) =>{
     })
 })
 
-router.put('/:id', mw.validateProjectId , (req,res) =>{
+router.put('/:id', mw.validateProjectId, mw.validateProject, (req,res) =>{
     Projects.update(req.params.id, req.body)
     .then((project) =>{
         res.status(200).json(project)
     })
     .catch((error) =>{
         res.status(500).json({message: 'Error updating project'})
+    })
+})
+
+router.delete('/:id', mw.validateProjectId, (req, res) =>{
+    Projects.remove(req.params.id)
+    .then((project) =>{
+        res.status(200).json({message: 'Project has been deleted'})
+    })
+    .catch((error) =>{
+        res.status(500).json({message: 'Error deleting project'})
     })
 })
 
