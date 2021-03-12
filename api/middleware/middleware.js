a5ab711bb486face924616431c0fb466994ee614
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-03-12 09:25:59 -0700

    validateActionId middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,6 +1,18 @@
 const Action = require("../actions/actions-model");
 
-const validateActionId = async (req, res, next) => {};
+const validateActionId = async (req, res, next) => {
+  try {
+    const action = await Action.getById(req.params.id);
+    if (!action) {
+      res.status(404).json({ message: "action not found" });
+    } else {
+      req.action = action;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
 const validateAction = (req, res, next) => {};
 
