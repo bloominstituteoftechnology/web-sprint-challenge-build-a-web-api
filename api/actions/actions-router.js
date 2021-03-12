f33f4a3547293347500789c2ba8d4a1290966dcd
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-03-12 09:14:09 -0700

    actions router scaffold

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,35 @@
 // Write your "actions" router here!
+const express = require("express");
+const Action = require("./actions-model");
+
+const router = express.Router();
+
+router.get("/", (req, res next) => {
+    
+})
+
+router.get("/:id", (req, res, next) => {
+    
+})
+
+router.post("/", (req, res) => {
+    
+})
+
+router.put("/:id", (req, res, next) => {
+    
+})
+
+router.delete("/:id", (req, res, next) => {
+    
+})
+
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+    custom: "something went wromg in the users router",
+  });
+});
+
+module.exports = router;
