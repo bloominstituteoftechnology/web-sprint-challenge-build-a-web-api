39efba76dd36dcb5613dc44547bcab67e44ec7d9
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-03-12 09:29:51 -0700

    validateAction middleware

---
+++ api/middleware/actions-middleware.js
@@ -0,0 +1,32 @@
+const Action = require("../actions/actions-model");
+
+const validateActionId = async (req, res, next) => {
+  try {
+    const action = await Action.getById(req.params.id);
+    if (!action) {
+      res.status(404).json({ message: "action not found" });
+    } else {
+      req.action = action;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+const validateAction = (req, res, next) => {
+  if (!req.body) {
+    res.status(400).json({ message: "missing action data" });
+  } else if (!req.body.description || !req.body.description.trim()) {
+    res.status(400).json({ message: "missing required description field" });
+  } else if (!req.body.notes || !req.body.notes.trim()) {
+    res.status(400).json({ message: "missing required notes field" });
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  validateActionId,
+  validateAction,
+};
