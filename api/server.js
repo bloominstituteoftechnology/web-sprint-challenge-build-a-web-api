5104a13cbf56595555fc17852b36fe3390fd9873
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-12 11:39:47 -0500

    imported the projects router get ready for creating endpoints and testing

--- api/server.js
+++ api/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const server = express();
 
 const actionsRouter = require('./actions/actions-router');
+const projectRouter = require('./projects/projects-router')
 
 server.use(express.json());
 
 server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectRouter)
 
 server.get('*', (req, res) => {
   res.status(200).json({
