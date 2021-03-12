fdb7d0f5bb75b4a3c9d826938e1034344f46a719
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-12 10:23:58 -0500

    create first end point to retrieve all actions export router to server.js

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -5,4 +5,16 @@ const { validateActionsId } = require('../middleware/middleware')
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  Action.get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: 'Could not retrieve Actions',
+      });
+    });
+});
+
 module.exports = router;
