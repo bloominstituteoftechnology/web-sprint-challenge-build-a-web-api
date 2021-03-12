86f3bac052bd7e7621fabec24293f550b71e5a22
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-03-12 09:32:27 -0700

    getting actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,28 +1,28 @@
 // Write your "actions" router here!
 const express = require("express");
+const {
+  validateActionId,
+  validateAction,
+} = require("../middleware/actions-middleware");
 const Action = require("./actions-model");
 
 const router = express.Router();
 
-router.get("/", (req, res next) => {
-    
-})
+router.get("/", (req, res, next) => {
+  Action.get()
+    .then((actions) => {
+      res.json(actions);
+    })
+    .catch(next);
+});
 
-router.get("/:id", (req, res, next) => {
-    
-})
+router.get("/:id", (req, res, next) => {});
 
-router.post("/", (req, res) => {
-    
-})
+router.post("/", (req, res) => {});
 
-router.put("/:id", (req, res, next) => {
-    
-})
+router.put("/:id", (req, res, next) => {});
 
-router.delete("/:id", (req, res, next) => {
-    
-})
+router.delete("/:id", (req, res, next) => {});
 
 router.use((err, req, res, next) => {
   res.status(500).json({
