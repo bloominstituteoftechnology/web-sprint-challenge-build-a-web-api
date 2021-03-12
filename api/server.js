040cb02ec2d0c3c8cefe31a88060766549892ee3
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-12 10:26:03 -0500

    import actions router to server test first endpoint on postman

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const server = express();
 
+const actionsRouter = require('./actions/actions-router');
+
+server.use(express.json());
+
+server.use('/api/actions', actionsRouter);
+
 server.get('*', (req, res) => {
   res.status(200).json({
     message: 'Welcome To My Sprint Submission',
