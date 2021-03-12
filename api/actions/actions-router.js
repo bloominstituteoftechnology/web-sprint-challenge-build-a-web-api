790a34dc2fd9df1cf8201139766e11d9ef8b8831
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-12 10:12:30 -0500

    create middleware to validate Actions id test in postman

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,2 +1,21 @@
 const Action = require('../actions/actions-model')
 const Project = require('../projects/projects-model')
+
+async function validateActionsId(req, res, next) {
+  const { id } = req.params;
+  try {
+    const action = await Action.get(id);
+    if (!action) {
+      res.status(404).json({
+        message: `Action with the ID ${id} does not exist`,
+      });
+    } else {
+      req.action = action;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'Could not Retrieve Actions',
+    });
+  }
+}
