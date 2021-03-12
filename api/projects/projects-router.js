06cb6c3efed456e661f58c59bad338f4e374bf42
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-12 11:50:05 -0500

    first endpoint tested successful on postman

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const Project = require('./projects-model')
+const { validateProjectsId } = require('../middleware/middleware')
 
 const router = express.Router();
+
+router.get('/', (req, res) => {
+  Project.get()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'Could Not Retrieve Projects',
+      });
+    });
+});
 
 module.exports = router;
