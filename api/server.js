6ed6bdc256470673f3b5682cbb0b17376dbd9230
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-03-21 23:37:12 -0600

    working

--- api/server.js
+++ api/server.js
@@ -1,15 +1,16 @@
+
 const express = require('express');
 const server = express();
 
-// Complete your server here!
-// Do NOT `server.listen()` inside this file!
-
 const projectsRouter = require('./projects/projects-router');
-const actionsRouter = require('/.actions/actions-router');
+const actionsRouter = require('./actions/actions-router');
 
 server.use(express.json());
 
 server.use('/api/projects', projectsRouter);
 server.use('/api/actions', actionsRouter);
+
+// Complete your server here!
+// Do NOT `server.listen()` inside this file!
 
 module.exports = server;
