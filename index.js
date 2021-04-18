7d9292926ee30ed2bd22131c5b0e514bc0fd980e
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2021-04-18 15:10:40 -0700

    installed the nodemon

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const server = require("./api/server");
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
