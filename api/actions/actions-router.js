9732f273aadc6cdc2e89bf5467dddcce31f011c1
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-05-07 10:59:10 -0400

    mvp done,implemented middleware on projects created router foro projects and action and also started the server, installed al dependencis such as cors and nodemon, express, and created custom scrips in the package.json file

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,51 @@
 // Write your "actions" router here!
+const express = require("express");
+const Action = require("./actions-model");
+const { checkDatabase, validateAction } = require("./actions-middleware");
+
+// Write your "actions" router here!
+const router = express.Router();
+
+router.use("/:id", checkDatabase);
+
+// [GET]
+router.get("/", (req, res, next) => {
+	Action.get()
+		.then((actions) => {
+			res.status(200).json(actions);
+		})
+		.catch((err) => next(err));
+});
+
+router.get("/:id", (req, res) => {
+	res.status(200).json(req.action);
+});
+
+// [POST]
+router.post("/", validateAction, (req, res, next) => {
+	Action.insert(req.body)
+		.then((action) => {
+			res.status(201).json(action);
+		})
+		.catch((err) => next(err));
+});
+
+// [PUT]
+router.put("/:id", validateAction, (req, res, next) => {
+	Action.update(req.params.id, req.body)
+		.then((updatedAction) => {
+			res.status(200).json(updatedAction);
+		})
+		.catch((err) => next(err));
+});
+
+// [DELETE]
+router.delete("/:id", (req, res, next) => {
+	Action.remove(req.params.id)
+		.then(() => {
+			res.status(200).json({ message: "Action deleted" });
+		})
+		.catch((err) => next(err));
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,43 @@
-const express = require('express');
-const server = express();
-
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+const actionsRouter = require("./actions/actions-router");
+const projectsRouter = require("./projects/projects-router");
+
+const server = express();
+
+// middlewares
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+// server error middleware
+server.use((err, req, res, next) => {
+	// eslint-disable-line
+	res
+		.status(500)
+		.json({
+			error: err.message,
+			message: "Something happened with the server",
+		});
+});
+
+// routers
+server.use("/api/actions", actionsRouter);
+server.use("/api/projects", projectsRouter);
+
+// defaults
+server.use("/api", (_, res) => {
+	res.json({ data: "API is accounted for" });
+});
+
+server.use("/", (req, res) => {
+	res.send(`<h2>Welcome to our API!</h2>`);
+});
 
 module.exports = server;
