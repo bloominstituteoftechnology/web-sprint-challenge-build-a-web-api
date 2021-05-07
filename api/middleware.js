51ee1a204471a957079d44e368e4b33a4fe3bcda
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-07 12:23:14 -0500

    build validateID

--- api/middleware.js
+++ api/middleware.js
@@ -1,3 +1,6 @@
+const Projects = require("./projects/projects-model");
+const Actions = require("./actions/actions-model");
+
 const logger = (req, res, next) => {
   console.log(`{
   request-type: ${req.method},
@@ -8,8 +11,26 @@ const logger = (req, res, next) => {
   next();
 };
 
-const validateID = (req, res, next) => {
-  next();
+const validateID = (req, _res, next) => {
+  const url = req.originalUrl;
+  const id = req.params.id;
+  url.startsWith("/api/projects")
+    ? Projects.get(id)
+        .then((result) => {
+          result
+            ? (req.idResult = result)
+            : next({ status: 404, message: "PARSE THIS" });
+        }) //!extrapolate this string
+        .catch(next())
+    : url.startsWith("/api/actions")
+    ? Actions.get(id)
+        .then((result) => {
+          result
+            ? (req.idResult = result)
+            : next({ status: 404, message: "PARSE THIS" });
+        }) //!extrapolate this string
+        .catch(next())
+    : next();
 };
 
 const validateBody = (req, res, next) => {
--- api/server.js
+++ api/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { logger, handleErrors } = require("./middleware");
+const { logger, handleErrors, validateID } = require("./middleware");
 const server = express();
 
 server.use(express.json());
@@ -8,7 +8,7 @@ server.use(logger);
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 
-server.use(handleErrors);
+server.use("/api/actions/:id", validateID);
 
 server.use("*", (_req, res) => {
   res.send("<h2>API located at /api</h2>");
