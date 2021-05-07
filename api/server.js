de13fe63034a3c65ec8064ab6c0aa3ae1489686e
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-07 15:47:10 -0500

    put on helmet

--- api/server.js
+++ api/server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const { logger, handleErrors } = require("./middleware");
 const projectsRouter = require("./projects/projects-router");
 const actionsRouter = require("./actions/actions-router");
+const helmet = require("helmet");
 const server = express();
 
+server.disable("x-powered-by");
+server.use(helmet());
 server.use(express.json());
 server.use(logger);
 server.use("/api/projects", projectsRouter);
