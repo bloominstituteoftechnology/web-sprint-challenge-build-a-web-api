50823873e8fb8c0f458742abb4e29b9f971d36f6
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-07 11:38:40 -0500

    remove endpoints message

--- api/server.js
+++ api/server.js
@@ -5,18 +5,10 @@ const server = express();
 server.use(express.json());
 server.use(logger);
 
-const projects = "/api/projects";
-const actions = "/api/actions";
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 
 server.use(handleErrors);
-
-server.use("/api", (_req, res) => {
-  res.send(
-    `<h2>Valid endpoints</h2><ul><li>${projects}</li><li>${actions}</li></ul>`
-  );
-});
 
 server.use("*", (_req, res) => {
   res.send("<h2>API located at /api</h2>");
