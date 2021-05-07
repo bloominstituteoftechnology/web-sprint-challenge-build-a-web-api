c0085c12bb44371a9434e182517b73742a632c5f
abe-one <77995299+abe-one@users.noreply.github.com>
2021-05-07 12:55:36 -0500

    build middleware

--- api/middleware.js
+++ api/middleware.js
@@ -1,5 +1,6 @@
 const Projects = require("./projects/projects-model");
 const Actions = require("./actions/actions-model");
+const { projectsSchema, actionsSchema } = require("./yupSchemas");
 
 const logger = (req, res, next) => {
   console.log(`{
@@ -34,11 +35,35 @@ const validateID = (req, _res, next) => {
 };
 
 const validateBody = (schema, body, next) => {
+  schema
+    .validate(body, { stripUnknown: true })
+    .then((validation) => {
+      body = validation;
+      next();
+    })
+    .catch((err) => next({ status: 400, message: err.message }));
+};
+
+const validateProject = (req, res, next) => {
+  validateBody(projectsSchema, req.body, next);
+};
+const validateAction = (req, res, next) => {
+  validateBody(actionsSchema, req.body, next);
+};
+
+const handleErrors = (err, req, res, next) => {
+  res.status(err.status || 500).json({
+    note: "DEVS: There's trouble afoot, let's solve it!",
+    message: err.message,
+    stack: err.stack,
+  });
   next();
 };
 
-const handleErrors = (error, req, res, next) => {
-  next();
+module.exports = {
+  logger,
+  validateID,
+  validateProject,
+  validateAction,
+  handleErrors,
 };
-
-module.exports = { logger, validateID, validateBody, handleErrors };
