2c9b488c21d30055fa678f8737a64013cf64246b
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-06-04 11:24:03 -0400

    set up get request in projects-router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,18 @@
 // Write your "projects" router here!
+const express = require("express");
+const router = express.Router();
+
+const Projects = require("./projects-model");
+
+//[GET] /api/projects
+router.get("/", (req, res) => {
+  Projects.get()
+    .then((project) => {
+      res.json(project);
+    })
+    .catch((err) => {
+      res.status(404).json({ message: err.message });
+    });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
 const server = express();
+server.use(express.json());
+
+const projectsRouter = require("./projects/projects-router");
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.use("/api/projects", projectsRouter);
 
 module.exports = server;
