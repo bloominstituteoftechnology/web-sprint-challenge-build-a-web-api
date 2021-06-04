7193170ac17e2f161f7df7ccc434abb623ccfdaa
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-06-04 11:51:13 -0400

    created get request to find project by id

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -15,4 +15,18 @@ router.get("/", (req, res) => {
     });
 });
 
+// `[GET] /api/projects/:id`
+router.get("/:id", async (req, res) => {
+  try {
+    const projectById = await Projects.get(req.params.id);
+    if (!projectById) {
+      res.status(404).json({ message: "no project found" });
+    } else {
+      res.json(projectById);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "problem finding user" });
+  }
+});
+
 module.exports = router;
