525dc52325b4ca8e6c3f9c74c6d272a8282137f5
Anwar <AnwarAldogmi@gmail.com>
2021-06-04 13:08:10 -0700

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,59 @@
 // Write your "projects" router here!
+const express = require('express')
+
+const router = express.Router()
+
+const Projects = require('./projects-model')
+const Actions = require('../actions/actions-model')
+
+router.get('/', async (req, res) => {
+    try {
+        const projects = await Projects.get()
+        res.json(projects)
+    }
+    catch(err) {
+        res.status(500).json({
+            message: 'failed to fetch projects'
+        })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const project = await Projects.get(id)
+        if (!project) {
+            res.status(404).json({
+                message: 'The specified project ID does not exist'
+            })
+        }else {
+            res. status(200).json(project)
+        }
+    }
+    catch(err) {
+        res.status(500).json({
+            error: {err}
+        })
+    }
+})
+
+router.post('/projects', async (req, res) => {
+    const body = req.body
+    if (!body.name || !body.description) {
+        res.status(400).json('Name & description required')
+    }else {
+        try{
+            const project = await Projects.insert(body)
+            res.status(201).json(project)
+        }
+        catch(err) {
+            res.status(500).json({
+                error: {err}
+            })
+        }
+    }
+})
+
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const server = express();
 
+const projectsRouter = require('./projects/projects-router')
+
+server.use(express.json())
+
+server.use('/api/projects', projectsRouter);
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Yoooo</h1>`)
+})
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
