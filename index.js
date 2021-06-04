5dcf60664e7607c4938326794517426c2739af40
MichaelHabermas <michaelghabermas@gmail.com>
2021-06-04 10:00:45 -0400

    sprint

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const path = require('path');
 const server = require('./api/server');
 
-server.listen(6000, () => {
-	console.log('\n*** Server Running on http://localhost:6000 ***\n');
+server.use(cors());
+server.use(express.static(path.join(__dirname, 'client/build'))); // static assets
+
+console.log(process.env.USER); // env USER=michael
+console.log(process.env.SHELL); // env SHELL=/bin/zhs
+
+if (process.env.NODE_ENV === 'production') {
+	console.log('this means this code is deployed');
+}
+
+const PORT = process.env.PORT || 6000;
+
+console.log('port is -> ', PORT);
+
+server.listen(PORT, () => {
+	console.log(`\n*** Server Running on http://localhost:${PORT}***\n`);
 });
