60e1b5a7588fa0143ae71c46eae6ef44e3b3500b
Rhiannon Stanford <mochi@Rhiannons-iMac.local>
2021-06-11 17:21:39 -0700

    Add to server, export router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,35 @@
 // Write your "projects" router here!
+const Projects = require('./posts-model');
+const express = require('express');
+const router = express.Router();
+
+/* from PROJECT MODEL
+module.exports = {
+  get,
+  insert,
+  update,
+  remove,
+  getProjectActions,
+};
+*/
+
+
+// PROJECTS ENDPOINTS
+
+router.get('/', (req, res) => {
+    Projects.get(req.query)
+        .then(response => {
+            console.log("response: ", response)
+            res.status(200).json(response);
+        })
+        .catch(error => {
+            console.log("error",error)
+            res.status(500).json({
+                message: 'Error retrieving the projects.',
+            });
+        });
+}); 
+
+router.get('/:id', (req, res) => {
+
+})
--- api/server.js
+++ api/server.js
@@ -6,4 +6,23 @@ const server = express();
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
+
+
+const projectsRouter = require('./projects/projects-router.js');
+const actionsRouter = require('./actions/actions-router.js');
+
+server.use(express.json());
+server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectsRouter);
+
+
+
+server.get('/', (req, res) => {
+    res.send(`
+      <h2>Server is working</h>
+    `);
+  });
+
+
+
 module.exports = server;
