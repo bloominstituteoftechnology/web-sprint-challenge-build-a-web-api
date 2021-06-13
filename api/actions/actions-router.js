070e365f01fad395de0efdee626f77a05c1d3f87
Rhiannon Stanford <mochi@Rhiannons-iMac.local>
2021-06-13 16:12:42 -0700

    Add delete to actions router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -99,4 +99,37 @@ router.put('/:id', (req, res) => {
     }
 });
 
+router.delete('/:id', (req,res) => {
+    const id = req.params.id;
+    console.log("id from router delete: ", id)
+
+
+    Actions.get(id)
+    
+        .then((response) => {
+            console.log("response: ", response); // response is action with given id
+            if (response) {
+           
+                console.log("found the action to delete: ", response)
+                Actions.remove(id)
+                    .then((deleteResponse) => {
+                        console.log("deleteResponse: ", deleteResponse)
+                        res.status(200).json({ message:  `Success! Delete action of id ${id}`})
+                    })
+                    .catch ((error) => {
+                        console.log("error", error)
+                        res.status(400).json({ message: `Unable to delete action.`})
+                    })
+
+            } else {
+                res.status(404).json({ message: `Action of ${id} not found`})
+            }
+        })
+        .catch((error) => {
+            console.log("error", error)
+            res.status(500).json({ message: `Error, the action of id ${id} could not be deleted: ${error}`})
+        })
+
+});
+
 module.exports = router;
