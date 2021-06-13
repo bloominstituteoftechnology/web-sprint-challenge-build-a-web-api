e86fc307cae9d7c9b00fb589a47f656a431b330c
Rhiannon Stanford <mochi@Rhiannons-iMac.local>
2021-06-13 15:59:44 -0700

    WIP

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -32,5 +32,31 @@ router.get('/', (req, res) => {
         });
 }); 
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    console.log("id: ", id)
+
+    Actions.get()
+    .then((actions) => {
+        console.log("actions: ", actions); // response is an array with all actions
+        if (actions) {
+            const filteredActions = actions.filter((action => {
+                console.log("Action project id: ", action.project_id)
+                console.log("id: ", id)
+                return action.project_id === Number(id);
+            }));
+            console.log("filteredActions: ",filteredActions)
+            res.status(200).json(filteredActions);
+        } else {
+            res.status(404).json([]);
+        }
+    })
+    .catch((error) => {
+        console.log("error", error)
+        res.status(500).json({ message: `Error, the project of id ${id} could not be deleted: ${error}`})
+    })
+
+});
+
 
 module.exports = router;
