450ada931ef1e63d4a7fc3a787205444d81e0b1d
Rhiannon Stanford <mochi@Rhiannons-iMac.local>
2021-06-13 16:03:37 -0700

    Adjust actions router for get by id

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -33,29 +33,22 @@ router.get('/', (req, res) => {
 }); 
 
 router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    console.log("id: ", id)
 
-    Actions.get()
-    .then((actions) => {
-        console.log("actions: ", actions); // response is an array with all actions
-        if (actions) {
-            const filteredActions = actions.filter((action => {
-                console.log("Action project id: ", action.project_id)
-                console.log("id: ", id)
-                return action.project_id === Number(id);
-            }));
-            console.log("filteredActions: ",filteredActions)
-            res.status(200).json(filteredActions);
-        } else {
-            res.status(404).json([]);
-        }
-    })
-    .catch((error) => {
-        console.log("error", error)
-        res.status(500).json({ message: `Error, the project of id ${id} could not be deleted: ${error}`})
-    })
-
+    Actions.get(req.params.id)
+    
+        .then((response) => {
+            console.log("response: ", response); // response
+            if (!response) {
+                res.status(404).json({ message: `Action of ${req.params.id} not found`})
+            } else {
+                console.log("we are in the else")
+                res.status(200).json(response);
+            }
+        })
+        .catch((error) => {
+            // console.log("error", error)
+            res.status(500).json({ message: `Error retrieving the project of id ${req.params.id}: ${error.message}`})
+        })
 });
 
 
