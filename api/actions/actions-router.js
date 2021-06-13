833ba813675df325f95d1a4b9e1e8093cd7c9721
Rhiannon Stanford <mochi@Rhiannons-iMac.local>
2021-06-13 16:07:34 -0700

    Add post to actions router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -51,5 +51,27 @@ router.get('/:id', (req, res) => {
         })
 });
 
+router.post('/', (req, res) => {
+    const body = req.body;
+    console.log("req.body: ", body)
+
+
+    if (!body || !body.project_id || !body.description || !body.notes ) {
+        res.status(400).json({ message: `All action fields required.`})
+    } else if (body.completed === null) {
+        res.status(400).json({ message: `All action fields required.`})
+    } else {
+
+        Actions.insert(req.body)
+            .then((response) => {
+                console.log("response", response) // response is posted action
+                res.status(200).json(response);
+            })
+            .catch((error) => {
+                console.log("error", error)
+                res.status(500).json({ message: `Error retrieving the action of id ${req.params.id}: ${error.message}`})
+            })
+    }
+});
 
 module.exports = router;
