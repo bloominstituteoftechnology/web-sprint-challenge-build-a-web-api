55c2527830426fc4e16c2994587a82dd38fb6608
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-06-22 15:36:15 +0200

    update deps

--- knexfile.js
+++ knexfile.js
@@ -1,56 +1,30 @@
+const commonConfig = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys = ON", done)
+    },
+  },
+  migrations: {
+    directory: "./data/migrations",
+  },
+  seeds: {
+    directory: "./data/seeds",
+  },
+}
+
 module.exports = {
   development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
+    ...commonConfig,
     connection: {
       filename: "./data/lambda.db3",
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      },
-    },
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
     },
   },
   testing: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
+    ...commonConfig,
     connection: {
       filename: './data/test.db3',
     },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
-    },
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-  },
-  production: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./data/lambda.db3",
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      },
-    },
-    migrations: {
-      directory: "./data/migrations",
-    },
-    seeds: {
-      directory: "./data/seeds",
-    },
-  },
-};
+  }
+}
