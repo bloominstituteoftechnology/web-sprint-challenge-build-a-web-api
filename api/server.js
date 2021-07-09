3a21d1a46695c33dea71e3e3cef818dd9f624541
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-07-09 11:10:47 -0600

    setting up server and index.js

--- api/server.js
+++ api/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const server = express();
+const actionRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router');
+
+
+server.use(express.json());
+// server.use('/api/actions', actionRouter);
+// server.use('/api/projects', projectsRouter);
+
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Lets write some code</h1>`);
+})
 
 module.exports = server;
