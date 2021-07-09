23eeb1a36e42d1063d1ecee8da6c05fbf0d703bf
wzd200 <william.z.dye@gmail.com>
2021-07-09 11:10:47 -0400

    Server is up and running

--- api/server.js
+++ api/server.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const server = express();
+const express = require('express')
+const server = express()
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.get('/', (req, res) => {
+    res.send(`<h2>Hello!</h2>`)
+ })
 
-module.exports = server;
+module.exports = server
