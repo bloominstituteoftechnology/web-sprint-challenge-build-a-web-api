05f9fcc899c71bad7b2f593328059109736c2351
Stan T <stan@Stans-Mac-mini.local>
2021-07-18 20:15:53 -0700

    server up-n-running

--- api/server.js
+++ api/server.js
@@ -1,9 +1,28 @@
-const express = require('express');
+/* eslint-disable */
+const express = require("express");
 const server = express();
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+server.get("/", (req, res, next) => {
+  res.send(`Server is up and running!`);
+});
+server.get("/api", (req, res) => {
+  res.json({ message: "api is working" });
+});
+server.get("/api/projects", (req, res) => {
+  res.json({ message: "projects here" });
+});
+server.get("/api/actions", (req, res) => {
+  res.json({ message: "actions here" });
+});
+server.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message, stack: err.stack });
+});
+server.use(function (req, res) {
+  res.status(404).send("Page not found");
+});
 
 module.exports = server;
