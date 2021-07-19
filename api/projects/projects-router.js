f6d95ee137155f0038047edfe9190d6e3ff5dd4c
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-07-18 20:41:33 -0400

    mvp

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,88 @@
 // Write your "projects" router here!
+const express = require('express')
+const Project = require('./projects-model')
+const {
+    validateProjectId,
+    validateProject,
+} = require('./projects-middleware')
+
+const router = express.Router()
+
+//GET
+
+router.get('/', async (req, res, next) => {
+try {
+        const users = await Project.get()
+        res.status(200).json(users)
+} catch (error) {
+        next(error)
+}
+})
+
+//GET
+
+router.get('/:id', validateProjectId, (req, res, next) => {
+try {
+        res.json(req.project)
+} catch (error) {
+        next(error)
+}
+})
+
+//POST
+
+router.post('/', validateProject, async (req, res, next) => {
+    try {
+        const newProject = await Project.insert({
+            name: req.name,
+            description: req.description,
+            completed: req.completed
+})
+        res.status(201).json(newProject)
+} catch (error) {
+        next(error)
+}
+})
+
+//PUT
+
+router.put('/:id', validateProjectId, validateProject, (req, res, next) => {
+Project.update(req.params.id, {
+    name: req.name,
+    description: req.description,
+    completed: req.completed
+})
+    .then(() => {
+            return Project.get(req.params.id)
+})
+    .then(project => {
+            res.json(project)
+})
+    .catch(next)
+})
+
+//DELETE
+
+router.delete('/:id', validateProjectId, async (req, res, next) => {
+try {
+    await Project.remove(req.params.id)
+        res.json(res.Project)
+} catch (error) {
+        next(error)
+}
+ })
+ 
+//GET
+
+router.get('/:id/actions', validateProjectId, async (req, res, next) => {
+Project.getProjectActions(req.params.id)
+    .then(actions => {
+    if (actions.length > 0) {
+        res.status(200).json(actions)
+} else {
+        res.status(404).json((actions))
+}
+})
+    .catch(next)
+})
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,9 +1,40 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const server = express();
-
+const projectsRouter = require('./projects/projects-router')
+const actionsRouter = require('./actions/actions-router')
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+function logger(req, res, next) {
+    console.log(`${req.method} request`)
+    next()
+  }
 
+const notFound = (req, res, next) => {
+    res.status(404).json({
+        message: 'Not found, sorry!'
+    })
+}
+
+const errorHandling = (err, req, res, next) => {
+    const status = err.status || 500
+    res.status(status).json({
+        message: err.message,
+    })
+}
+
+server.use(express.json())
+
+server.use('/api/projects', logger, projectsRouter)
+server.use('/api/actions', logger, actionsRouter)
+
+server.use('*', notFound)
+
+server.use(errorHandling)
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Hello!</h2>`)
+ })
 module.exports = server;
