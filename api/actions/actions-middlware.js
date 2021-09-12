fff7bfefa5a2edeb80ae2ea65c6477b0a0190409
drewmrodriguez <drewmrodriguez@gmail.com>
2021-09-12 13:11:24 -0500

    initial commit

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,2 @@
 // add middlewares here related to actions
+
--- index.js
+++ index.js
@@ -12,3 +12,8 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+const server = require('./api/server')
+
+server.listen(5000, () => {
+    console.log('Server is running on port 5000')
+})
