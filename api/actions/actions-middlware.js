5b5b0c21103f5c00d5856331720a846607fe11a2
Stan <stan2dor@gmail.com>
2021-09-12 14:09:42 -0700

    Routers, Middlewares

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,48 @@
-// add middlewares here related to actions
+const Action = require("../actions/actions-model");
+
+const validatePost = (req, res, next) => {
+  if (!req.body.notes || !req.body.description || !req.body.project_id) {
+    next({
+      status: 400,
+      message: "missing required field",
+    });
+  } else {
+    next();
+  }
+};
+
+const validateActionId = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const action = await Action.get(id);
+    console.log(action);
+    if (action) {
+      req.action = action;
+      next();
+    } else {
+      next({
+        status: 404,
+        message: "action not found",
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+const validateAction = async (req, res, next) => {
+  if (!req.body.notes || !req.body.description || !req.body.project_id) {
+    next({
+      status: 400,
+      message: "missing required field",
+    });
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  validatePost,
+  validateActionId,
+  validateAction,
+};
--- api/server.js
+++ api/server.js
@@ -1,15 +1,39 @@
+/* eslint-disable */
 const express = require("express");
 const server = express();
 
+// Do NOT `server.listen()` inside this file!
 server.use(express.json());
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+const actionsRouter = require("./actions/actions-router");
+const projectsRouter = require("./projects/projects-router");
 
+// const { logger, errorHandling } = require("../api/actions/actions-middlware");
+
+server.use("/api/actions", actionsRouter);
+server.use("/api/projects", projectsRouter);
+
+// server.use("/", logger);
+// server.use(errorHandling);
+
+server.get("/", (req, res) => {
+  res.send(`This is Sparta!`);
+});
 server.get("/api", (req, res) => {
   res.json({ message: "api is working" });
+});
+server.get("/api/projects", (req, res) => {
+  res.json({ message: "projects here" });
+});
+server.get("/api/actions", (req, res) => {
+  res.json({ message: "actions here" });
+});
+server.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(500).json({ message: err.message, stack: err.stack });
+});
+server.use(function (req, res) {
+  res.status(404).send("404 - Page not found");
 });
 
 module.exports = server;
