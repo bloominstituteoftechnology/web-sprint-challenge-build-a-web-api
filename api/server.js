d52acf0dd8641d8f7e9fdb9bf75f444b75657546
Stan <stan2dor@gmail.com>
2021-09-12 14:35:25 -0700

    Test PASS

--- api/server.js
+++ api/server.js
@@ -8,13 +8,8 @@ server.use(express.json());
 const actionsRouter = require("./actions/actions-router");
 const projectsRouter = require("./projects/projects-router");
 
-// const { logger, errorHandling } = require("../api/actions/actions-middlware");
-
 server.use("/api/actions", actionsRouter);
 server.use("/api/projects", projectsRouter);
-
-// server.use("/", logger);
-// server.use(errorHandling);
 
 server.get("/", (req, res) => {
   res.send(`This is Sparta!`);
@@ -28,12 +23,9 @@ server.get("/api/projects", (req, res) => {
 server.get("/api/actions", (req, res) => {
   res.json({ message: "actions here" });
 });
-server.use((err, req, res, next) => {
-  // eslint-disable-line
-  res.status(500).json({ message: err.message, stack: err.stack });
-});
+
 server.use(function (req, res) {
-  res.status(404).send("404 - Page not found");
+  res.status(404).send("404 - This Page not found");
 });
 
 module.exports = server;
