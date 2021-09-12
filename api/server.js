06a9c8f79e55b113112d9ac9c35387c482e58d46
Stan <stan2dor@gmail.com>
2021-09-12 13:44:41 -0700

    project & server setup

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
 const server = express();
+
+server.use(express.json());
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.get("/api", (req, res) => {
+  res.json({ message: "api is working" });
+});
 
 module.exports = server;
--- index.js
+++ index.js
@@ -12,3 +12,10 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+const server = require("./api/server");
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server running on ${port} ***\n`);
+});
