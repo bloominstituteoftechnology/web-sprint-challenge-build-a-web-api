e5f1b11e94c0cbb22d81681c279acb519408dec9
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-09-12 17:22:48 -0700

    working on actions router, adding mw for :id

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,16 @@
 // add middlewares here related to actions
+const Actions = require('../actions/actions-model.js')
+
+const checkActionId = async (req,res,next)=>{
+    const {id}=req.params
+    const action = await Actions.get(id)
+    if(!action){
+        res.status(404).json({ message: "action not found" })
+    }else{
+        next()
+    }
+}
+
+module.exports={
+    checkActionId
+} 
