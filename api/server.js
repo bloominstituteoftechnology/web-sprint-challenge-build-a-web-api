e7f1dff6516a61078b9d29292bae9904b9f2d317
cyamos2 <cyamos2@yahoo.com>
2021-09-13 01:36:08 -0400

    finished for now

--- api/server.js
+++ api/server.js
@@ -1,9 +1,29 @@
-const express = require('express');
-const server = express();
+const express = require('express')
+const server = express()
+const projectsRouter = require('./projects/projects-router')
+const actionsRouter = require('./actions/actions-router')
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+function logger(req, res, next) {
+    console.log(`${req.method} request`)
+    next()
+  }
 
-module.exports = server;
+const errorHandling = (err, req, res, next) => { // eslint-disable-line
+    const status = err.status || 500
+    res.status(status).json({
+        message: err.message,
+    })
+}
+
+server.use(express.json())
+
+server.use('/api/projects', logger, projectsRouter)
+server.use('/api/actions', logger, actionsRouter)
+
+server.use(errorHandling)
+
+server.get('/', (req, res) => {
+    res.send(`<h2>API is running</h2>`)
+ })
+
+module.exports = server
