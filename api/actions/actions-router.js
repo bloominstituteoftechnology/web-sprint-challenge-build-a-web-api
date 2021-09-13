702724799fe8a90fb68c0bc8626dc464bea9b5cf
nicholastorr <nick.torres213@gmail.com>
2021-09-12 23:44:23 -0400

    complete

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,119 @@
 // Write your "actions" router here!
+const router = require('express').Router()
+const Actions = require('./actions-model')
+
+
+router.get('/', (req,res,next) =>{
+    Actions.get()
+            .then(actions=>res.json(actions))
+            .catch(err => next(err))
+})
+
+
+  router.get('/:id', async (req, res) => {
+    try {
+        const action = await Actions.get(req.params.id)
+        console.log('-->', action)
+        if(!action){
+            res.status(404).json({
+                message: "The action with the specified ID does not exist. Array starts at Nr2"
+            })
+        } else {
+            res.json(action)
+        }
+    } catch (err){
+        res.status(500).json({
+            message: "The action with the specified ID does not exist",
+            err: err.message,
+            stack: err.stack
+    })
+    }
+})
+
+router.put('/:id', (req,res, next) => {
+    const {notes, description,} = req.body
+    if (!notes || !description ) {
+        res.status(400).json({
+            message: "The action with the specified ID does not exist. Ensure correct ID. Array may start at #2"
+        })
+    } else {
+        Actions.update(req.params.id, req.body)
+        .then( ()=> {
+            return Actions.get(req.params.id)
+        })
+        .then(action=>{
+            res.json(action)
+        })
+        .catch(next)
+    }
+})
+
+
+// put 1.1 - with middleware
+router.put("/:id", (req, res, next) => {
+    const id = req.params.id
+    const body = req.body
+    Actions.get(id)
+        .then(() => {
+            return Actions.update(id, body)
+        })
+        .then(() => {
+            return Actions.get(id)
+        })
+        .then(action => res.json(action))
+        .catch(err => next(err))
+});
+
+
+
+
+router.delete('/:id', async (req,res) => { // EDIT NOTE: add user id validation later
+    try {
+
+        const action = await Actions.get(req.params.id)
+        if (!action) {
+            console.log(action)
+            res.status(404).json({
+                message: "The projet with the specified ID does not exist"
+            })
+        } else {
+
+            await Actions.remove(req.params.id)
+            res.json(action, req.action)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The action could not be removed, but there may be one.",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+})
+
+
+
+router.delete("/:id", async (req, res, next) => {
+    const id = req.params.id
+    try{
+        const actionId = await Actions.get(id)
+        if(actionId){
+            await Actions.remove(id)
+            res.json(actionId)
+            next()
+        }
+    }catch(err){
+        next(err)
+    }
+});
+
+
+router.use((err, req, res) => {
+    res.status(500).json({
+      message: "Error",
+      error: err.message
+    });
+  });
+
+
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -15,4 +15,119 @@ router.get('/', (req, res, next) => {
         .catch(error => next(error));
     });
 
+
+
+    router.delete('/:id', async (req,res) => { // EDIT NOTE: add user id validation later
+        try {
+            //throw new Error('aight')
+            const project = await Projects.get(req.params.id)
+            if (!project) {
+                console.log(project)
+                res.status(404).json({
+                    message: "The projet with the specified ID does not exist"
+                })
+            } else {
+                //console.log(project)
+                await Projects.remove(req.params.id)
+                res.json(project, req.project)
+            }
+        } catch (err) {
+            res.status(500).json({
+                message: "The project could not be removed, but there may be one.",
+                err: err.message,
+                stack: err.stack,
+            })
+        }
+    })
+
+
+    router.post('/', (req,res) => {
+        const {name, description, completed} = req.body
+        if (!name || !description) {
+            res.status(400).json({
+                message: "Please provide the name and description for the project"
+            })
+        } else {
+            console.log('success')
+            Projects.insert({ name, description, completed})
+            .then ( ({id}) => {
+                console.log('check -->', id)
+                return Projects.get(id)
+            })
+            .then(project => {
+                res.status(201).json(project)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "There was an error while creating and saving the project to the database",
+                    err: err.message,
+                    stack: err.stack
+                })
+            })
+        }
+    })
+
+
+
+router.get('/:id', async (req, res) => {
+
+    try {
+        const project = await Projects.get(req.params.id)
+        console.log('-->', project)
+        if(!project){
+            res.status(404).json({
+                message: "The project with the specified ID does not exist. Array starts at Nr2"
+            })
+        } else {
+            res.json(project)
+        }
+    } catch (err){
+        res.status(500).json({
+            message: "The project with the specified ID does not exist",
+            err: err.message,
+            stack: err.stack
+    })
+    }
+})
+
+
+
+
+router.put('/:id', (req,res, next) => {
+    const {name, description, completed} = req.body
+    if (!name || !description || !completed) {
+        res.status(400).json({
+            message: "The project with the specified ID does not exist. Ensure correct ID. Array may start at #2"
+        })
+    } else {
+        Projects.update(req.params.id, req.body)
+        .then( ()=> {
+            return Projects.get(req.params.id)
+        })
+        .then(project=>{
+            res.json(project)
+        })
+        .catch(next)
+    }
+})
+
+
+
+
+
+
+
+router.put("/:id", (req, res, next) => { // //eslint-disable-line
+    const id = req.params.id
+    const body = req.body
+    Projects.get(id)
+        .then(() => {
+            return Projects.update(id, body)
+        })
+        .then(() => {
+            return Projects.get(id)
+        })
+        .then(project => res.json(project))
+        .catch(err => next(err))
+});
 module.exports = router;
