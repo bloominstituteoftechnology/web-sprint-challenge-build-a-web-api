2387d685ea4fb7ea11463ff0d4b262c9153ab001
nicholastorr <nick.torres213@gmail.com>
2021-09-12 23:29:50 -0400

    error installing sqlite3

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,18 @@
 // Write your "projects" router here!
+const express = require("express");
+
+const Projects = require("./projects-model");
+
+const router = express.Router();
+
+router.use(express.json());
+
+router.get('/', (req, res, next) => {
+    Projects.get()
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(error => next(error));
+    });
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const server = express();
 
+const projectsRouter = require('./projects/projects-router');
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
 module.exports = server;
+
+
+//seed: nodeProj3, nodeProj2
+
+
+// remember express by default cannot parse JSON in request bodies
+server.use(express.json())
+
+
+// global middlewares and the user's router need to be connected here
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`)
+})
+
+server.use('/projects', projectsRouter)
