7644584901b1020bfa8ea0e6a7e6e1e434063fe0
Klove Adams <kloveadams@gmail.com>
2021-09-30 18:50:36 -0400

    pulled server into index.js and it is listening

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+require("dotenv").config();
+
+const server = require('./api/server.js');
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`listening on ${port}`)
+});
+
