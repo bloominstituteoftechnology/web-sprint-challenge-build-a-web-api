a526ce1b11abc2cae72eded2eb4d76e5cf73c0f7
christina-yun <poncheeyun@yahoo.com>
2021-09-30 16:48:04 -0700

    Project 2/2 get endpoints finished

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,24 @@
-// add middlewares here related to projects
+const yup = require('yup');
+const Projects = require('./projects-model');
+
+async function validateProjectId(req, res, next) {
+    try{
+        const validatedId = await Projects.get(req.params.id);
+
+        if(!validatedId){
+            next({ status: 404, message:'project not found'});
+        } else {
+            req.user = validatedId;
+            next();
+        }
+    
+    }
+    catch(err){
+        next(err);
+    }
+};
+
+
+module.exports = {
+    validateProjectId
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,19 +1,24 @@
 const express = require('express');
 
+const { validateProjectId } = require('./projects-middleware');
 const Projects = require('./projects-model');
 
 const router = express.Router();
 
 // [GET] /api/projects
-// Returns an array of projects as the body of the response.
-// If there are no projects it responds with an empty array.
-router.get('/', (req, res) =>{});
-
+router.get('/', (req, res, next) => {
+    Projects.get()
+        .then(projectArray => {
+            
+            res.status(200).json(projectArray);
+        })
+        .catch(next)
+});
 
 //  [GET] /api/projects/:id
-// Returns a project with the given id as the body of the response.
-// If there is no project with the given id it responds with a status code 404.
-router.get('/:id', (req, res) => {});
+router.get('/:id', validateProjectId, (req, res) => {
+    res.status(200).json(req.user);
+});
 
 //  [POST] /api/projects
 // Returns the newly created project as the body of the response.
