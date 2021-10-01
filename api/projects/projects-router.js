3ba112e2a334c04b98b6aa17f24a116f9a1ad4cc
stevenjhomem <sjhomem@outlook.com>
2021-10-01 09:15:27 -0500

    able to perform the first get request and get my first two tests to pass

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -4,5 +4,26 @@ const Actions = require('../actions/actions-model');
 
 const router = express.Router();
 
+//get requests//
+
+//first request: Returns an array of projects as the body of the response and if there are no projects it responds with an empty array
+router.get('/', async (req, res)=>{
+    try{
+        const projects = await Projects.get();
+        res.status(400).json(projects);
+    }
+    catch(err){
+        res.status(500).json({
+            message: "There was an issue accessing the server with your information"
+        })
+    }
+})
+//get requests//
+
+
+
+
+
+
 // do not forget to export the router
 module.exports=router;
