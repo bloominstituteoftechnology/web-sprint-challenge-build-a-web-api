c3c3c70a940c1ef00b23206a73f3624485a097a1
ehsan2793 <ehsan2793@gmail.com>
2021-10-01 13:59:28 -0700

    more clean up dry code

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,47 +1,37 @@
-
 const express = require('express');
 const Action = require('./actions-model');
-const router = express.Router()
+const router = express.Router();
 
 const { checkId, validateAction } = require('./actions-middlware');
 
-
 router.get('/', async (req, res, next) => {
     try {
-        const allActions = await Action.get()
-        res.status(200).json(allActions)
-
+        const allActions = await Action.get();
+        res.status(200).json(allActions);
     } catch (err) {
-        next(err)
+        next(err);
     }
-})
-
+});
 
 router.get('/:id', checkId, (req, res) => {
-    res.status(200).json(req.found)
-})
+    res.status(200).json(req.found);
+});
 
 router.post('/', validateAction, async (req, res) => {
-
-    const actionA = await Action.insert(req.body)
-    res.status(201).json(actionA)
-})
+    const actionA = await Action.insert(req.body);
+    res.status(201).json(actionA);
+});
 
 router.put('/:id', checkId, validateAction, async (req, res) => {
+    const actionA = await Action.update(req.params.id, req.body);
 
-
-    const actionA = await Action.update(req.params.id, req.body)
-
-    res.status(200).json(actionA)
-})
+    res.status(200).json(actionA);
+});
 
 router.delete('/:id', checkId, async (req, res) => {
-    const actionA = await Action.remove(req.params.id, req.body)
+    const actionA = await Action.remove(req.params.id, req.body);
 
-    res.status(200).json(actionA)
-})
-
-
-
+    res.status(200).json(actionA);
+});
 
 module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,64 +1,51 @@
 const Project = require('./projects-model');
 const express = require('express');
 
-const router = express.Router()
-const { checkId, validateProject, validateChange } = require('./projects-middleware');
+const router = express.Router();
+const {
+    checkId,
+    validateProject,
+    validateChange,
+} = require('./projects-middleware');
+
 router.get('/', async (req, res, next) => {
     try {
-        const allProjects = await Project.get()
-        res.status(200).json(allProjects)
+        const allProjects = await Project.get();
+        res.status(200).json(allProjects);
     } catch (err) {
-        next(err)
+        next(err);
     }
-})
+});
 
 router.get('/:id', checkId, (req, res) => {
-    res.status(200).json(req.found)
-})
+    res.status(200).json(req.found);
+});
 
 router.post('/', validateProject, async (req, res) => {
-    const newProject = await Project.insert(req.body)
-    res.status(201).json(newProject)
-
-
-})
+    const newProject = await Project.insert(req.body);
+    res.status(201).json(newProject);
+});
 
 router.put('/:id', checkId, validateChange, async (req, res) => {
-    const updated = await Project.update(req.params.id, req.body)
-    res.status(200).json(updated)
-})
+    const updated = await Project.update(req.params.id, req.body);
+    res.status(200).json(updated);
+});
 
 router.delete('/:id', checkId, async (req, res) => {
-    await Project.remove(req.params.id)
-    res.status(200).json([{ message: " ⭐️⭐️⭐️ project blow has been deleted ⭐️⭐️⭐️", deleted_project: req.found }])
-})
+    await Project.remove(req.params.id);
+    res
+        .status(200)
+        .json([
+            {
+                message: ' ⭐️⭐️⭐️ project blow has been deleted ⭐️⭐️⭐️',
+                deleted_project: req.found,
+            },
+        ]);
+});
 
-router.get('/:id/actions', checkId, async (req, res, next) => {
-    const projectActions = await Project.getProjectActions(req.params.id)
-    try {
-        if (req.found.actions === []) {
-            res.status(200).json([])
-        }
-        else {
-            res.status(200).json(projectActions)
-        }
-    } catch (error) {
-        next()
-    }
-
-
-})
-
+router.get('/:id/actions', checkId, async (req, res) => {
+    const projectActions = await Project.getProjectActions(req.params.id);
+    res.status(200).json(projectActions);
+});
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
