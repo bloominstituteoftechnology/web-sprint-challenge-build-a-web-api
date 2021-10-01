0c841d7820886a7d24d950b4b7976899cdf74b15
ehsan2793 <ehsan2793@gmail.com>
2021-09-30 22:49:29 -0700

    action can be added to specific project

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,17 +1,17 @@
 // add middlewares here related to actions
-const { get } = require('./actions-model');
-
+const Action = require('./actions-model');
+const Post = require('../projects/projects-model')
+const yup = require('yup');
 
 const checkId = async (req, res, next) => {
     try {
         const id = req.params.id;
 
-        const found = await get(id);
+        const found = await Action.get(id);
 
         if (!found) {
             res.status(404).json({ message: "sorry that that id doesn't exist'" });
         } else {
-            console.log(found)
             req.found = found;
             next();
         }
@@ -20,9 +20,39 @@ const checkId = async (req, res, next) => {
     }
 };
 
+const vaidatetion = yup.object().shape({ //eslint-disable-line
+    notes: yup.string().required(),
+    description: yup.string().required(),
+    project_id: yup.number().required()
+});
+
+const validateAction = async (req, res, next) => {
+    const id = req.body.project_id;
+
+    const findPost = await Post.get(1)
+    const validatebody = await vaidatetion.isValid(req.body);
+    console.log(req.body);
+    try {
+        if (!findPost)
+            res.status(404).json({ message: `Sorry we coundt find a project with that id of ${id}` })
+        if (!validatebody) {
+            res.status(400).json({ message: "action is missing id or note or description" })
+        }
+        else {
+            next();
+        }
+    } catch (error) {
+        next(error);
+    }
+
+
+
+};
 
 
 module.exports = {
     checkId,
+    validateAction,
+
 
 };
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const Action = require('./actions-model');
 const router = express.Router()
 
-const { checkId } = require('./actions-middlware');
+const { checkId, validateAction } = require('./actions-middlware');
 
 
 router.get('/', async (req, res, next) => {
@@ -21,8 +21,10 @@ router.get('/:id', checkId, (req, res) => {
     res.status(200).json(req.found)
 })
 
-router.post('/', (req, res) => {
+router.post('/', validateAction, (req, res) => {
 
+    Action.insert(req.body)
+    res.status(200).json(req.body)
 })
 
 router.put('/:id', (req, res) => {
