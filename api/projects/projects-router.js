4bf636f593e2ff7d9e71be9605819555435f88a4
stevenjhomem <sjhomem@outlook.com>
2021-10-01 08:59:31 -0500

    set up projectRouter and have it sending information to server.js

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,8 @@
-// Write your "projects" router here!
+const express = require("express");
+const Projects = require('./projects-model');
+const Actions = require('../actions/actions-model');
+
+const router = express.Router();
+
+// do not forget to export the router
+module.exports=router;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const projectRouter = require('./projects/projects-router');
 
 
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json());
+server.use('/api/projects/', projectRouter)
 
 
 // global middlewares and the user's router need to be connected here
