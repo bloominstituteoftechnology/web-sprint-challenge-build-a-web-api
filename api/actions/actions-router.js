b151d972f9b3b586cb00435c0413f431704b4522
ehsan2793 <ehsan2793@gmail.com>
2021-09-30 23:54:20 -0700

    actions can be deleted and all test are passing

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -24,7 +24,6 @@ router.get('/:id', checkId, (req, res) => {
 router.post('/', validateAction, async (req, res) => {
 
     const actionA = await Action.insert(req.body)
-    // Action.insert(req.body)
     res.status(200).json(actionA)
 })
 
@@ -36,8 +35,10 @@ router.put('/:id', validateAction, async (req, res) => {
     res.status(200).json(actionA)
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', checkId, async (req, res) => {
+    const actionA = await Action.remove(req.params.id, req.body)
 
+    res.status(200).json(actionA)
 })
 
 
