5e1cb8ba0eef6b49a90922feec0e18e137102f66
stevenjhomem <sjhomem@outlook.com>
2021-10-01 12:07:53 -0500

    finished two more action tests

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,24 @@
-// add middlewares here related to actions
+const Actions = require('./actions-model');
+
+async function validateActionsId(req, res, next) {
+    try{
+      const action = await Actions.get(req.params.id)
+      if(!action){
+        res.status(404).json({
+          message: "No action can be found with that ID"
+        })
+      }
+      else{
+        req.actions = action//this saves another trip to the database by tacking on the user to the request object for the next middleware function
+        next()
+      }
+    }
+    catch(err){
+      res.status(500).json({
+        message: "Having trouble accessing the database"
+      })
+    }
+  }
+
+
+module.exports = {validateActionsId}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { validateActionsId } = require("./actions-middlware");
 const Actions = require('./actions-model');
 
 const actionRouter = express.Router();
@@ -15,6 +16,19 @@ actionRouter.get('/', async (req, res)=>{
         res.status(500).json({
             message: "There was an issue accessing the server with your information"
         })
+    }
+})
+
+//second request: Returns an action with the given id as the body of the response. If there is no action with the given id it responds with a status code 404.
+
+actionRouter.get('/:id', validateActionsId, async (req, res, next)=>{
+    try{
+        const id = req.params.id;
+        const actionFromId = await Actions.get(id);
+        res.status(200).json(actionFromId);
+    }
+    catch(err){
+        next(err)
     }
 })
 //**get requests**//
