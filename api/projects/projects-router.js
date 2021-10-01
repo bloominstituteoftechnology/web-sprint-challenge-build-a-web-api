bee0b31ee75082c42dcd2d0bf32d37bf21b30b09
Klove Adams <kloveadams@gmail.com>
2021-09-30 22:43:52 -0400

    Brain too tired for figuring out put

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -24,4 +24,20 @@ router.post('/', validProject, (req, res, next) => {
     .catch(next)
 });
 
+router.put('/:id', checkProjectId, validProject, (req, res, next) => {
+  const { id } = req.body;
+  Project.update(id, {
+    name: req.name, 
+    description: req.description, 
+    completed: req.completed 
+  })
+    .then(() => {
+      return Project.getById(id)
+    })
+    .then(updatedProject => {
+      res.json(updatedProject)
+    })
+    .catch(next)
+});
+
 module.exports = router;
