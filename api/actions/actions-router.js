f5ac1896e8e9bc209c7ba2a75e90d47cc4558cce
stevenjhomem <sjhomem@outlook.com>
2021-10-01 12:51:02 -0500

    finished actions post

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -9,8 +9,8 @@ const actionRouter = express.Router();
 //first request: Returns an array of actions (or an empty array) as the body of the response.
 actionRouter.get('/', async (req, res)=>{
     try{
-        const projects = await Actions.get();
-        res.status(200).json(projects);
+        const actions = await Actions.get();
+        res.status(200).json(actions);
     }
     catch(err){
         res.status(500).json({
@@ -38,7 +38,23 @@ actionRouter.get('/:id', validateActionsId, async (req, res, next)=>{
 
 //first request: Returns the newly created action as the body of the response. If the request body is missing any of the required fields it responds with a status code 400. When adding an action make sure the project_id provided belongs to an existing project.
 
-
+actionRouter.post('/', async (req,res,)=>{
+    try{
+        const {project_id, description, notes, completed}=req.body;
+        if(!project_id || !description || !notes || typeof completed === "undefined"){
+            res.status(400).json({message: " We need all information: name, description, and completed boolean value"})
+        }
+        else{
+            const updatedAction = await Actions.insert(req.body)
+            res.status(201).json(updatedAction)
+        }
+    }
+    catch(err){
+        res.status(500).json({
+            message: "There was an issue accessing the server with your information"
+        })
+    }
+})
 //**post requests**//
 
 
