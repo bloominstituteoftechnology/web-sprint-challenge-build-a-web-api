328421efed45beb82312bca7577fffcf0628cdf6
jarghiskhan <jared.l.wyatt@outlook.com>
2021-10-09 16:00:50 -0400

    completed actions passing tests

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,30 @@
 // add middlewares here related to actions
+const Actions = require("./actions-model")
+
+function validateActionId(req,res, next){
+    const {id}= req.params;
+
+    Actions.get(id)
+    .then((actions) => {
+      if (!actions) {
+        res.status(404).json([]);
+      } else {
+        req.actions = actions;
+        next();
+      }
+    })
+}
+
+function validateRequestBody(req, res, next){
+    const {notes, description, project_id} = req.body
+    if(!notes || !description || !project_id || Object.keys(req.body).length === 0){
+        res.status(400).json({message: "missing name, description or project ID field."})
+      }else{
+        next()
+      }
+}
+
+module.exports = {
+    validateActionId,
+    validateRequestBody
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -2,6 +2,73 @@
 const express = require("express");
 const Actions = require("./actions-model");
 const router = express.Router();
-console.log("actions running")
+const {
+  validateActionId,
+  validateRequestBody,
+} = require("./actions-middlware");
+console.log("actions running");
 
+router.get("/", (req, res) => {
+  Actions.get()
+    .then((action) => {
+      if (!action) {
+        res.status(404).json([]);
+      } else {
+        res.status(200).json(action);
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Unable to get Actions" });
+    });
+});
+
+router.get("/:id", validateActionId, (req, res) => {
+  const { id } = req.params;
+  Actions.get(id)
+    .then((action) => {
+      res.status(200).json(action);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Unable to get Projects" });
+    });
+});
+
+router.post("/", validateRequestBody, (req, res) => {
+  const newAction = req.body;
+  Actions.insert(newAction)
+    .then((action) => {
+      res.status(201).json(action);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "There was an error adding project." });
+    });
+});
+
+router.put("/:id", validateActionId, validateRequestBody, (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  if (req.body.completed === undefined) {
+    res.status(400).json({ message: "Requires the completed field." });
+  } else {
+    Actions.update(id, changes)
+      .then((action) => {
+        res.status(200).json(action);
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ message: "There was an error updating the user." });
+      });
+  }
+});
+
+router.delete("/:id", validateActionId,(req,res)=>{
+    const {id} = req.params;
+    Actions.remove(id)
+    .then((action)=>{
+        res.status(200).json(action)
+    }).catch(()=>{
+        res.status(500).json({message:"Unable to delete user."})
+    })
+})
 module.exports = router;
