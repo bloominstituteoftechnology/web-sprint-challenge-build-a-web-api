cf1fb9beaed527b8fa089df67cb7b28de941ad78
jarghiskhan <jared.l.wyatt@outlook.com>
2021-10-09 15:42:36 -0400

    added project middleware and finished more

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,30 @@
 // add middlewares here related to projects
+const Projects = require("./projects-model")
+
+function validateProjectId(req,res, next){
+    const {id}= req.params;
+
+    Projects.get(id)
+    .then((projects) => {
+      if (!projects) {
+        res.status(404).json([]);
+      } else {
+        req.projects = projects;
+        next();
+      }
+    })
+}
+
+function validateRequestBody(req, res, next){
+    const {name, description} = req.body
+    if(!name || !description || Object.keys(req.body).length === 0){
+        res.status(400).json({message: "missing name, description or completed field."})
+      }else{
+        next()
+      }
+}
+
+module.exports = {
+    validateProjectId,
+    validateRequestBody
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,41 +2,74 @@
 const express = require("express");
 const Projects = require("./projects-model");
 const router = express.Router();
-console.log("projects running")
+const {
+  validateProjectId,
+  validateRequestBody,
+} = require("./projects-middleware");
+console.log("projects running");
 
-router.get('/', (req, res)=>{
-    Projects.get()
-    .then((projects)=>{
-        if(!projects){
-            res.status(404).json([])
-        }else{
-           res.status(200).json(projects) 
-        }
-        
+router.get("/", (req, res) => {
+  Projects.get()
+    .then((project) => {
+      if (!project) {
+        res.status(404).json([]);
+      } else {
+        res.status(200).json(project);
+      }
     })
-    .catch(()=>{
-        res.status(500).json({message: 'Unable to get Projects'})
+    .catch(() => {
+      res.status(500).json({ message: "Unable to get Projects" });
+    });
+});
+
+router.get("/:id", validateProjectId, (req, res) => {
+  const { id } = req.params;
+  Projects.get(id)
+    .then((project) => {
+      res.status(200).json(project);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Unable to get Projects" });
+    });
+});
+
+router.post("/", validateRequestBody, (req, res) => {
+  const newProject = req.body;
+  Projects.insert(newProject)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch(() => {
+      res.status(500).json({ message: "There was an error adding project." });
+    });
+});
+
+router.put("/:id", validateProjectId, validateRequestBody, (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  if (req.body.completed === undefined) {
+    res.status(400).json({ message: "Requires the completed field." });
+  } else {
+    Projects.update(id, changes)
+      .then((project) => {
+        res.status(200).json(project);
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ message: "There was an error updating the user." });
+      });
+  }
+});
+
+router.delete("/:id", validateProjectId,(req,res)=>{
+    const {id} = req.params;
+    Projects.remove(id)
+    .then((project)=>{
+        res.status(200).json(project)
+    }).catch(()=>{
+        res.status(500).json({message:"Unable to delete user."})
     })
 })
-
-router.get('/:id', (req, res)=>{
-    const {id}= req.params
-    console.log(id)
-    Projects.get(id)
-    .then((projects)=>{
-        if(!projects){
-            res.status(404).json([])
-        }else{
-           res.status(200).json(projects) 
-        }
-        
-    })
-    .catch(()=>{
-        res.status(500).json({message: 'Unable to get Projects'})
-    })
-})
-
-
 
 module.exports = router;
-
