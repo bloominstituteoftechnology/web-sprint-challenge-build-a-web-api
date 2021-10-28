07629631d5bf33a5eb34a0b402eaa07b4e40a9d6
IrvinA <arevalosirvin@gmail.com>
2021-10-28 15:56:04 -0700

    wip

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -3,11 +3,24 @@ const Projects = require('../projects/projects-model');
 
 function handleError(err, req, res, next) {
     res.status(err.status || 500).json({
-      message: err.message,
-      prodMessage: 'Theres an error and this handles it',
+      message: err.message
     })
 }
 
+function validateID (req, res, next) {
+  Projects.get(req.params.id)
+    .then(project => {
+      if (project) {
+        req.project = project
+        next()
+      } else {
+        next({ status: 404, message: 'Project not found'})
+      }
+    })
+    .catch(next)
+}
+
 module.exports = {
-    handleError
+    handleError,
+    validateID
 }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,18 +1,38 @@
 // Write your "projects" router here!
 const express = require('express');
 
+const {
+    handleError,
+    validateID
+} = require('./projects-middleware')
+
 const Projects = require('../projects/projects-model');
 
 const router = express.Router();
 
 //[GET] /api/projects
+router.get('/', (req, res, next) => {
+    // RETURN AN ARRAY WITH ALL THE USERS
+    Projects.get()
+      .then(projects => {
+        res.status(200).json(projects)
+      })
+      .catch(next)
+  });
 
 //[GET] /api/projects/:id
+router.get('/:id', validateID, (req, res, next) => {
+    res.status(200).json(req.project)
+})
 
-//[GET] /api/projects/:id
+//[POST] /api/projects
 
 //[PUT] /api/projects/:id
 
 //[DELETE] /api/projects/:id
 
 //[GET] /api/projects/:id/actions
+
+router.use(handleError);
+
+module.exports = router;
