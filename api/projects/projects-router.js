4dc5b495e2d5865d63c4fbf731c82e596e9ed404
kennedybryant <kennedy_marisa96@yahoo.com>
2021-10-28 20:32:56 -0400

    Get Project By Id

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -16,7 +16,31 @@ router.get('/', (req, res) => {
                 res.status(200).json([])
             }
         })
-        .catch()
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: "The projects could not be received"
+            })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    Projects.get(req.params.id)
+        .then(project => {
+            if(project) {
+                res.status(200).json(project)
+            } else {
+                res.status(404).json({
+                    message: "Could not find a project with the given id"
+                })
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: "The projects could not be received"
+            })
+        })    
 })
 
 module.exports = router;
