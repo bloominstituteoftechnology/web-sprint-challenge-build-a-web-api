5d33d6c25fb3083ecafd82d6a95cc6b579453c7e
beefybroccoli <tedkimemail@yahoo.com>
2021-10-29 03:42:22 -0700

    initial

--- api/server.js
+++ api/server.js
@@ -1,5 +1,10 @@
-const express = require('express');
+const express = require("express");
 const server = express();
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello world" });
+});
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
--- index.js
+++ index.js
@@ -12,3 +12,11 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+require("dotenv").config();
+const server = require("./api/server");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log("======================================");
+  console.log(`listening on port ${PORT}`);
+});
