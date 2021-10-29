3beaf278778149fc040016e5be44f989c4de036e
kennedybryant <kennedy_marisa96@yahoo.com>
2021-10-28 20:25:23 -0400

    Setup && Get Projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,22 @@
 // Write your "projects" router here!
+const express = require('express')
+
+const Projects = require('./projects-model')
+
+const router = express.Router()
+
+router.use(express.json())
+
+router.get('/', (req, res) => {
+    Projects.get()
+        .then(projects => {
+            if (projects) {
+                res.status(200).json(projects)
+            } else {
+                res.status(200).json([])
+            }
+        })
+        .catch()
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -12,3 +12,12 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+const dotenv = require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+
+const server = require('./api/server')
+
+server.listen(PORT, () => {
+    console.log(`\n****Listening on port ${PORT}****\n`)
+})
