e4f2683579e96f19a2ac10dc5009020f098becd5
beefybroccoli <tedkimemail@yahoo.com>
2021-10-29 04:18:51 -0700

    ...progress

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -8,21 +8,21 @@ const router = express.Router();
 //   - Returns an array of projects as the body of the response.
 //   - If there are no projects it responds with an empty array.
 router.get("/", async (req, res) => {
-  res.status(100).json({ message: `[GET] /api/projects` });
+  res.status(200).json({ message: `[GET] /api/projects/` });
 });
 
 // - [ ] `[GET] /api/projects/:id`
 //   - Returns a project with the given `id` as the body of the response.
 //   - If there is no project with the given `id` it responds with a status code 404.
 router.get("/:id", async (req, res) => {
-  res.status(100).json({ message: `[GET] /api/projects/:id` });
+  res.status(200).json({ message: `[GET] /api/projects/${req.params.id}` });
 });
 
 // - [ ] `[POST] /api/projects`
 //   - Returns the newly created project as the body of the response.
 //   - If the request body is missing any of the required fields it responds with a status code 400.
 router.post("/", async (req, res) => {
-  res.status(100).json({ message: `[POST] /api/projects` });
+  res.status(201).json({ message: `[POST] /api/projects/` });
 });
 
 // - [ ] `[PUT] /api/projects/:id`
@@ -30,21 +30,23 @@ router.post("/", async (req, res) => {
 //   - If there is no project with the given `id` it responds with a status code 404.
 //   - If the request body is missing any of the required fields it responds with a status code 400.
 router.put("/:id", async (req, res) => {
-  res.status(100).json({ message: `[PUT] /api/projects/:id` });
+  res.status(201).json({ message: `[PUT] /api/projects/${req.params.id}` });
 });
 
 // - [ ] `[DELETE] /api/projects/:id`
 //   - Returns no response body.
 //   - If there is no project with the given `id` it responds with a status code 404.
 router.delete("/:id", async (req, res) => {
-  res.status(100).json({ message: `[DELETE] /api/projects/:id` });
+  res.status(201).json({ message: `[DELETE] /api/projects/${req.params.id}` });
 });
 
 // - [ ] `[GET] /api/projects/:id/actions`
 //   - Returns an array of actions (could be empty) belonging to a project with the given `id`.
 //   - If there is no project with the given `id` it responds with a status code 404.
 router.get("/:id/actions", async (req, res) => {
-  res.status(100).json({ message: `[GET] /api/projects/:id/actions` });
+  res
+    .status(200)
+    .json({ message: `[GET] /api/projects/${req.params.id}/actions` });
 });
 
 module.exports = router;
