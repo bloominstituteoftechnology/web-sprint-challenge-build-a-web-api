346fa80ca8e3f2d065bc14abb3ac7bb1e1902404
IrvinA <arevalosirvin@gmail.com>
2021-10-28 18:49:58 -0700

    removed some unneeded comments

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,4 +1,3 @@
-// Write your "projects" router here!
 const express = require('express');
 
 const {
--- api/server.js
+++ api/server.js
@@ -6,11 +6,6 @@ const actionsRouter = require('./actions/actions-router');
 const server = express();
 server.use(express.json());
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
-
 server.use('/api/projects', projectsRouter);
 server.use('/api/actions', actionsRouter);
 
