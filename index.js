51c0e515d06dd65d162c9ec3b255a43cb5525e62
Aidan Jenkins <aidan.jenkins@gmail.com>
2021-12-12 13:39:38 -0800

    complete mvp

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ Pull your server into this file and start it!
 */
 const server = require('./api/server')
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
     console.log("server is running my guy")
